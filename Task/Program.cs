8b46033f952d5a929b33b0d1dee751b8c7ddf5fb
Valeria28-10 <valeriakonovalenko828@gmail.com>
2023-06-28 12:12:05 +0300

    Task solved

---
+++ Task/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] firstArray = new string[] {"Russia", "Denmark", "Kazan", "1234", "-2"};
+string[] secondArray = new string[firstArray.Length];
+
+void SelectionArraySort(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+    if(firstArray[i].Length <= 3)
+        {
+        secondArray[count] = firstArray[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SelectionArraySort(firstArray, secondArray);
+Console.Write($"[{String.Join(", " , firstArray)}] -> ");
+PrintArray(secondArray);
+
